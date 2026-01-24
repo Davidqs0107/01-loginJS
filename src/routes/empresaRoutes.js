1b80a9ae460e7ccca8618d8f11874794d53c9176
david <davidqs0107@gmail.com>
2026-01-24 12:44:10 -0400

    feat: add role validation middleware to updateEmpresa route for enhanced security

--- src/routes/empresaRoutes.js
+++ src/routes/empresaRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { getEmpresaById, getSummary, getSummaryCobrador, updateEmpresa } from "../controllers/empresaController.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
+import { validarRol } from "../middlewares/validar-rol.js";
+import { userRol } from "../constants/usuarios.constants.js";
+const { superAdmin, admin } = userRol;
+
 const route = Router();
 route.use(validarJWT);
 
@@ -8,5 +12,5 @@ route.get('/', getEmpresaById);
 route.get('/summary', getSummary);
 route.get('/summary/cobrador', getSummaryCobrador);
 
-route.put('/', updateEmpresa);
+route.put('/', [validarRol(superAdmin, admin)], updateEmpresa);
 export default route;
